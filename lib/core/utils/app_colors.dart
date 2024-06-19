5636fc7df77ccd6bda565ca96da71d6742886495
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-19 15:13:23 +0300

    App Strings & Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff292929);
+  static const Color scaffoldColor = Color(0xffF8F8F8);
+  static const Color favoriteGreyColor = Color(0xffEEEEEE);
+  static const Color seeAllColor = Color(0xffE6E6E6);
+  static const Color dotsColr = Color(0xffB8B8B8);
+  static const Color textfield = Color.fromRGBO(228, 228, 228, 0.52);
+}
---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,21 @@
+abstract class AppStrings {
+  //ToDo:All Strings
+
+  static const String appName = "Slash.",
+      nasrCity = "Nasr City",
+      cairo = "Cairo",
+      searchHere = "Search here..",
+      home = "Home",
+      profile = "Profile",
+      myCart = "My Cart",
+      favorites = "Favorites",
+      categories = "Categories",
+      seeAll = "See All",
+      fashion = "Fashion",
+      games = "Games",
+      accessories = "Accessories",
+      books = "Books",
+      bestSelling = "Best Selling",
+      newarrival = "New Arrival",
+      recommendedForYou = "Recommended For You";
+}
