62693fb7c30f92bc3f785e2c10a6324feac42b18
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-19 14:46:37 +0300

    App Assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,104 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBabyGirlDress
+  /// assets/images/BabyGirlDress.png
+  static const String imagesBabyGirlDress = "assets/images/BabyGirlDress.png";
+
+  /// Assets for imagesBanner1
+  /// assets/images/banner1.png
+  static const String imagesBanner1 = "assets/images/banner1.png";
+
+  /// Assets for imagesBanner2
+  /// assets/images/banner2.jpg
+  static const String imagesBanner2 = "assets/images/banner2.jpg";
+
+  /// Assets for imagesBookBlank
+  /// assets/images/BookBlank.png
+  static const String imagesBookBlank = "assets/images/BookBlank.png";
+
+  /// Assets for imagesBrand1
+  /// assets/images/brand1.png
+  static const String imagesBrand1 = "assets/images/brand1.png";
+
+  /// Assets for imagesBrand2
+  /// assets/images/brand2.png
+  static const String imagesBrand2 = "assets/images/brand2.png";
+
+  /// Assets for imagesBrand3
+  /// assets/images/brand3.png
+  static const String imagesBrand3 = "assets/images/brand3.png";
+
+  /// Assets for imagesDice
+  /// assets/images/Dice.png
+  static const String imagesDice = "assets/images/Dice.png";
+
+  /// Assets for imagesDogMedal
+  /// assets/images/DogMedal.png
+  static const String imagesDogMedal = "assets/images/DogMedal.png";
+
+  /// Assets for imagesFilter
+  /// assets/images/filter.png
+  static const String imagesFilter = "assets/images/filter.png";
+
+  /// Assets for imagesGlasses
+  /// assets/images/Glasses.png
+  static const String imagesGlasses = "assets/images/Glasses.png";
+
+  /// Assets for imagesHeart
+  /// assets/images/heart.png
+  static const String imagesHeart = "assets/images/heart.png";
+
+  /// Assets for imagesHome
+  /// assets/images/home.png
+  static const String imagesHome = "assets/images/home.png";
+
+  /// Assets for imagesLeatherBracelet
+  /// assets/images/LeatherBracelet.png
+  static const String imagesLeatherBracelet = "assets/images/LeatherBracelet.png";
+
+  /// Assets for imagesLeatherJacket
+  /// assets/images/LeatherJacket.png
+  static const String imagesLeatherJacket = "assets/images/LeatherJacket.png";
+
+  /// Assets for imagesLocation
+  /// assets/images/location.png
+  static const String imagesLocation = "assets/images/location.png";
+
+  /// Assets for imagesNotebook
+  /// assets/images/Notebook.png
+  static const String imagesNotebook = "assets/images/Notebook.png";
+
+  /// Assets for imagesNotifcationIcon
+  /// assets/images/NotifcationIcon.png
+  static const String imagesNotifcationIcon = "assets/images/NotifcationIcon.png";
+
+  /// Assets for imagesPrintedbag
+  /// assets/images/Printedbag.png
+  static const String imagesPrintedbag = "assets/images/Printedbag.png";
+
+  /// Assets for imagesProfileCircle
+  /// assets/images/profile-circle.png
+  static const String imagesProfileCircle = "assets/images/profile-circle.png";
+
+  /// Assets for imagesSearchNormal
+  /// assets/images/search-normal.png
+  static const String imagesSearchNormal = "assets/images/search-normal.png";
+
+  /// Assets for imagesShirt
+  /// assets/images/Shirt.png
+  static const String imagesShirt = "assets/images/Shirt.png";
+
+  /// Assets for imagesShoppingCart
+  /// assets/images/shopping-cart.png
+  static const String imagesShoppingCart = "assets/images/shopping-cart.png";
+
+  /// Assets for imagesStitchKeychain
+  /// assets/images/StitchKeychain.png
+  static const String imagesStitchKeychain = "assets/images/StitchKeychain.png";
+
+  /// Assets for imagesWoolenScarf
+  /// assets/images/WoolenScarf.png
+  static const String imagesWoolenScarf = "assets/images/WoolenScarf.png";
+}
+
