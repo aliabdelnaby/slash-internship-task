250d34e6d0ef2ad28ef26b88b4ef783112cc82ef
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-19 19:52:11 +0300

    Finish Slide Banners

--- lib/features/home/presentation/widgets/home_screen_mobile_layout.dart
+++ lib/features/home/presentation/widgets/home_screen_mobile_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:slash_task/features/home/presentation/widgets/custom_appbar.dart';
 import 'package:slash_task/features/home/presentation/widgets/custom_search_widget.dart';
+import 'package:slash_task/features/home/presentation/widgets/slide_banner.dart';
 
 class HomeScreenMobileLayout extends StatelessWidget {
   const HomeScreenMobileLayout({super.key});
@@ -17,7 +18,8 @@ class HomeScreenMobileLayout extends StatelessWidget {
               const CustomAppBar(),
               SizedBox(height: MediaQuery.of(context).size.height * 0.03),
               const CustomSearchWidget(),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+              const SlideBanners(),
             ],
           ),
         ),
@@ -25,3 +27,4 @@ class HomeScreenMobileLayout extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/features/home/presentation/widgets/slide_banner.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: library_private_types_in_public_api
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:slash_task/core/utils/app_assets.dart';
+import 'package:slash_task/core/utils/app_colors.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class SlideBanners extends StatefulWidget {
+  const SlideBanners({super.key});
+
+  @override
+  _SlideBannersState createState() => _SlideBannersState();
+}
+
+class _SlideBannersState extends State<SlideBanners> {
+  final CarouselController _controller = CarouselController();
+  int _current = 0;
+
+  final List<String> imgList = [
+    Assets.imagesBanner1,
+    Assets.imagesBanner2,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          items: imgList.map((item) => Image.asset(item)).toList(),
+          carouselController: _controller,
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            aspectRatio: 2.8,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _current = index;
+              });
+            },
+          ),
+        ),
+        SizedBox(height: MediaQuery.of(context).size.height * 0.01),
+        AnimatedSmoothIndicator(
+          activeIndex: _current,
+          count: imgList.length,
+          effect: const ScrollingDotsEffect(
+            activeDotColor: AppColors.primaryColor,
+            dotColor: AppColors.dotsColr,
+            dotHeight: 5,
+            dotWidth: 8,
+            activeDotScale: 1.5,
+          ),
+        ),
+      ],
+    );
+  }
+}
